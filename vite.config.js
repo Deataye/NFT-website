35003e36e2657eb3c214a4509a4de75ea0d63b97
Deataye <shahzaiba61@gmail.com>
2024-09-25 16:45:10 +0500

    resolving deployment errors

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['Hero'], // Replace 'module-name' with the actual module causing the error
+    },
+  },
 })
